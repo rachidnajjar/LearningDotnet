1e17d5600f05f29b00aba8350e7f14f4805796ad
rachidnajjar <rachid.najjar@sfr.fr>
2020-10-31 15:14:32 +0100

    Ajout d'un projet de test unitaire

---
+++ HelloCommandDotnet.Test/PipedInputTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloCommandDotnet.Test
+{
+    [TestClass]
+    public class PipedInputTests
+    {
+        [TestMethod]
+        public void PipedInput_Should_UnionWithUserSuppliedValues()
+        {
+        }
+    }
+}
