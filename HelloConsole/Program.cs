74a2c71b4ee1326f92eb61b3c761a5ca3a793e1f
rachidnajjar <rachid.najjar@sfr.fr>
2020-11-04 12:06:11 +0100

    Prise en charge des paramètres passés en argument

--- HelloConsole/Program.cs
+++ HelloConsole/Program.cs
@@ -6,7 +6,17 @@ namespace HelloConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Nombre de paramètres passés en argument : {args.Length}");
+
+
+            string param1 = "world";
+            if (args.Length > 0)
+            {
+                param1 = args[0];
+            }
+
+
+            Console.WriteLine($"Hello {param1}!");
         }
     }
 }
