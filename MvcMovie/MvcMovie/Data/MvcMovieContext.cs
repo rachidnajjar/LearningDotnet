736f56f5a2dc87326d9ba5975cdb467d5c10f067
rachidnajjar <rachid.najjar@sfr.fr>
2020-11-07 16:27:36 +0100

    Créer une appli web mvc avec asp.net core mvc

---
+++ MvcMovie/MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext()
+        {
+            
+        }
+
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
