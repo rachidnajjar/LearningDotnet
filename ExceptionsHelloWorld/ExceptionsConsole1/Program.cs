db5962c2509041a8c229f951a012cafcba7fc78b
rachidnajjar <rachid.najjar@sfr.fr>
2020-11-09 13:11:38 +0100

    Learning how to handle exceptions

---
+++ ExceptionsHelloWorld/ExceptionsConsole1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionsConsole1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader sr = File.OpenText("data.txt"))
+                {
+                    Console.WriteLine($"The first line of this file is {sr.ReadLine()}");
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"The file was not found:\r\n{e}");
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine($"The directory was not found:\r\n{e}");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"The file could not be opened:\r\n{e}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An exception occured:\r\n{e}");
+            }
+        }
+    }
+}
